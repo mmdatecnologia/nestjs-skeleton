d13c28b6ef7b655cd121a39de36b4d33c9a24712
mmdatecnologia <mmdatecnologia@gmail.com>
2021-09-18 20:48:48 -0300

    chore: config dev tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'standard-with-typescript'
   ],
   root: true,
   env: {
@@ -20,5 +21,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/comma-spacing": "off",
+    "@typescript-eslint/return-await": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "import/export": "off"
   },
 };
---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    roots: ['<rootDir>/tests'],
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.ts',
+      '!<rootDir>/src/main/**'
+    ],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    testEnvironment: 'node',
+    preset: '@shelf/jest-mongodb',
+    transform: {
+      '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+      '@/test/(.*)': '<rootDir>/test/$1',
+      '@/(.*)': '<rootDir>/src/$1'
+    },
+    moduleFileExtensions: [
+        "js",
+        "json",
+        "ts"
+      ],
+  }
