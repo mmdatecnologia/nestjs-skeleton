1c41a6f9749700365ab7cda7aee62f624db79c6f
MMDA Tecnologia <mmdatecnologia@gmail.com>
2021-09-20 15:27:26 -0300

    refactor: move scafolding files to app dir

---
+++ src/config/api.config.ts
@@ -0,0 +1,11 @@
+require('dotenv-flow').config();
+
+export const apiConfig = {
+  dataProvider: {
+    apiKey: process.env.DATA_PROVIDER_API_KEY,
+    idClient: process.env.DATA_PROVIDER_ID_CLIENT,
+    url: process.env.DATA_PROVIDER_URL,
+  },
+  serverUrl: process.env.SERVER_URL || 'http://localhost',
+  serverPort: process.env.SERVER_PORT || 3000,
+};
