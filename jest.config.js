d93f9a7e8973f09011b816f1d544a1012fb4a186
MMDA Tecnologia <mmdatecnologia@gmail.com>
2021-09-20 10:47:58 -0300

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,16 @@
 module.exports = {
-    roots: ['<rootDir>/tests'],
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.ts',
-      '!<rootDir>/src/main/**'
-    ],
-    coverageDirectory: 'coverage',
-    coverageProvider: 'babel',
-    testEnvironment: 'node',
-    preset: '@shelf/jest-mongodb',
-    transform: {
-      '.+\\.ts$': 'ts-jest'
-    },
-    moduleNameMapper: {
-      '@/test/(.*)': '<rootDir>/test/$1',
-      '@/(.*)': '<rootDir>/src/$1'
-    },
-    moduleFileExtensions: [
-        "js",
-        "json",
-        "ts"
-      ],
-  }
+  roots: ['<rootDir>/test'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/test/(.*)': '<rootDir>/test/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+};
